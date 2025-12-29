762d2f42c2e25c26e7cec75d79b6a1d7c8a5465b
d-yuriyovych <d.yuriyovych@gmail.com>
2025-12-29 21:26:52 +0200

    Update change_server.js

--- change_server.js
+++ change_server.js
@@ -11,6 +11,7 @@
 
     var icon_svg = '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/></svg>';
 
+    // ПОВНІСТЮ ЗБЕРЕЖЕНА ФУНКЦІЯ З ВЕРСІЇ 8
     function openServerModal() {
         var selected_url = null;
         var selected_name = null;
@@ -24,7 +25,6 @@
             </div>
         `);
 
-        // ПЕРЕВІРКА СТАТУСІВ ЯК У ВЕРСІЇ 8
         function checkStatus(url, el) {
             var img = new Image();
             img.onload = function() { $(el).text(' - Online').css('color', '#4caf50'); };
@@ -68,37 +68,40 @@
         });
     }
 
-    function startPlugin() {
-        // 1. ВСТАВКА В НАЛАШТУВАННЯ (БЕЗ SettingsApi.addParam ЩОБ НЕ БУЛО ПОМИЛКИ)
+    function init() {
+        // Твій метод реєстрації через SettingsApi
         Lampa.SettingsApi.addComponent({
             component: 'server_redirect_mod',
             name: 'Зміна сервера',
             icon: icon_svg
         });
 
-        // Слухаємо відкриття налаштувань і вставляємо пункт вручну
-        Lampa.Settings.listener.follow('open', function (e) {
-            if (e.name === 'server_redirect_mod') {
-                setTimeout(function() {
-                    var layer = $('.settings__layer').last();
-                    if (layer.length && !layer.find('.srv-custom-item').length) {
-                        var item = $(`
-                            <div class="settings__item selector srv-custom-item">
-                                <div class="settings__item-icon">${icon_svg}</div>
-                                <div class="settings__item-name">Відкрити список дзеркал</div>
-                                <div class="settings__item-descr">Натисніть для вибору іншого сервера Lampa</div>
-                            </div>
-                        `);
-                        item.on('click hover:enter', openServerModal);
-                        layer.append(item);
-                        // Оновлюємо контролер, щоб Lampa побачила новий селектор
-                        Lampa.Controller.enable('settings_server_redirect_mod');
-                    }
-                }, 50);
+        // Додаємо пункт, який просто викликає функцію openServerModal()
+        Lampa.SettingsApi.addParam({
+            component: 'server_redirect_mod',
+            param: {
+                name: 'trigger',
+                type: 'title'
+            },
+            field: {
+                name: 'Відкрити список дзеркал'
+            },
+            onChange: function() {
+                // Це для надійності
+                openServerModal();
             }
         });
 
-        // 2. HEADER + MENU (З 8-ї версії)
+        // Пряма вставка в налаштування, щоб уникнути помилки undefined
+        Lampa.Settings.listener.follow('open', function (e) {
+            if (e.name === 'server_redirect_mod') {
+                setTimeout(function() {
+                    $('.settings__item').addClass('selector').off('click').on('click', openServerModal);
+                }, 100);
+            }
+        });
+
+        // Кнопки в Header та Menu (З Версії 8)
         setInterval(function() {
             if (!$('.srv-head-btn').length && $('.head__actions').length) {
                 $('<div class="head__action selector srv-head-btn" style="margin-right:15px; cursor: pointer;">' + icon_svg + '</div>')
@@ -111,6 +114,6 @@
         }, 2000);
     }
 
-    if (window.appready) startPlugin();
-    else Lampa.Listener.follow('app', function(e) { if (e.type == 'ready') startPlugin(); });
+    if (window.Lampa) init();
+    else window.addEventListener('lampa_init', init);
 })();
