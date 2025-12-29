e0e56b2dfe51c0e46be743925597872af1e96c3b
d-yuriyovych <d.yuriyovych@gmail.com>
2025-12-29 22:00:31 +0200

    Update change_server.js

--- change_server.js
+++ change_server.js
@@ -1,15 +1,1 @@
-(function () {
-
-    if (!window.Lampa) return;
-
-    console.log('CHANGE SERVER PLUGIN LOADED');
-
-    Lampa.Menu.add({
-        title: 'TEST SERVER',
-        icon: 'network_check',
-        action: function () {
-            Lampa.Noty.show('Плагін працює ✔ Lampa 1.12.3');
-        }
-    });
-
-})();
+alert('PLUGIN LOADED');
